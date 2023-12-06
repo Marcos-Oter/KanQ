627da85915da6de73651c6831ebf312f6a7c2cfa
Marcos Otermin <marcosotermin@hotmail.com>
2023-12-06 12:19:18 -0300

    CRUD finalizado

--- kanq/app/urls.py
+++ kanq/app/urls.py
@@ -3,5 +3,8 @@ from . import views
 
 urlpatterns = [
     path("",views.index, name="index"),
-    path("insert/", views.insert, name="insert")
+    path("create/", views.create, name="create"),
+    path("update", views.update, name="update"),
+    path("update/<int:task_id>", views.update_form, name="update_form"),
+    path("delete/<int:task_id>", views.delete, name="delete")
 ]
--- kanq/app/views.py
+++ kanq/app/views.py
@@ -10,17 +10,56 @@ def index(request):
     #template = loader.get_template("app/index.html")
     db_data = task.objects.all()
     context = {
-        "db_data": db_data
+        "db_data": db_data[::-1],
+        "update": None
     }
     print(db_data)
     #return HttpResponse(template.render(context, request))
     return render(request, "app/index.html", context)
 
-def insert(request):
+def create(request):
+    try:
+        task_task = request.POST["task"]
+        task_description = request.POST["description"]
+        #Si el los campos de texto están vacíos no crea creará la tarea en base de datos.
+        if not task_task or not task_description:
+            raise ValueError("El texto no puede quedar vacío.")
+
+        db_data = task(task=task_task, description=task_description)
+        db_data.save()
+        #print(request.POST["task"])
+        #print(request.POST["description"])
+        return HttpResponseRedirect(reverse("index"))
+    except ValueError as err:
+        print(err)
+        return HttpResponseRedirect(reverse("index"))
+    
+def update(request):
+    task_id = request.POST["id"]
     task_task = request.POST["task"]
     task_description = request.POST["description"]
-    db_data = task(task=task_task, description=task_description)
+    db_data = task.objects.get(pk=task_id)
+    db_data.task = task_task
+    db_data.description = task_description
     db_data.save()
-    #print(request.POST["task"])
-    #print(request.POST["description"])
+    return HttpResponseRedirect(reverse("index"))
+    
+def update_form(request, task_id):
+    #template = loader.get_template("app/index.html")
+    db_data = task.objects.all()
+    db_data_only= task.objects.get(pk=task_id)
+
+    context = {
+        "db_data": db_data[::-1],
+        "update": db_data_only
+    }
+    print(db_data)
+    #return HttpResponse(template.render(context, request))
+    return render(request, "app/index.html", context)
+
+def delete(request, task_id):
+
+    db_data = task.objects.filter(id=task_id)
+    db_data.delete()
+
     return HttpResponseRedirect(reverse("index"))
