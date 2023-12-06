2a4bf83bfe10aa20b88dd12b92fd7b52ff147606
Marcos Otermin <marcosotermin@hotmail.com>
2023-12-06 01:33:22 -0300

    función Create

--- kanq/app/views.py
+++ kanq/app/views.py
@@ -1,11 +1,26 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-from django.template import loader
+from django.http import HttpResponse, HttpResponseRedirect
+from django.urls import reverse
+#from django.template import loader
+from . models import task
 
 # Create your views here.
 
 def index(request):
     #template = loader.get_template("app/index.html")
-    context = {}
+    db_data = task.objects.all()
+    context = {
+        "db_data": db_data
+    }
+    print(db_data)
     #return HttpResponse(template.render(context, request))
     return render(request, "app/index.html", context)
+
+def insert(request):
+    task_task = request.POST["task"]
+    task_description = request.POST["description"]
+    db_data = task(task=task_task, description=task_description)
+    db_data.save()
+    #print(request.POST["task"])
+    #print(request.POST["description"])
+    return HttpResponseRedirect(reverse("index"))
