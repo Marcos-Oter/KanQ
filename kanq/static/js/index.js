01d6d4a731da492171008cedb4116b134a659f4a
Marcos Otermin <marcosotermin@hotmail.com>
2023-12-10 10:26:43 -0300

    Depurando

--- kanq/static/js/index.js
+++ kanq/static/js/index.js
@@ -1,7 +1,8 @@
 const botonMostrarModal = document.getElementById('mostrarModal');
-const modal = document.getElementById('dialog');
 const botonCerrarModal = document.getElementById('cerrarModal');
-const botonCerrarModal2 = document.getElementById('cerrarModal2');
+const modal = document.getElementById('dialog');
+
+const form = document.querySelector("form");
 
 const showFormModal = ()=> 
 {
@@ -12,13 +13,8 @@ const showFormModal = ()=>
     botonCerrarModal.addEventListener('click', () => {
         modal.close();
     });
-
-    botonCerrarModal2.addEventListener('click', () => {
-        modal.close();
-    });
 }
-console.log("xddd");
+
 showFormModal();
 
 ScrollReveal().reveal('.scrollreveal', { delay: 75 });
-ScrollReveal().reveal('.scrollreveal2', { delay: 175 });
