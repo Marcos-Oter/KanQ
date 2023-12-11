7a40c99713d9b407f209b6498447e4990270fa65
Marcos Otermin <marcosotermin@hotmail.com>
2023-12-11 02:11:33 -0300

    Actualización a Nueva DATABASE

--- kanq/kanq/settings.py
+++ kanq/kanq/settings.py
@@ -79,11 +79,22 @@ WSGI_APPLICATION = 'kanq.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': BASE_DIR / 'db.sqlite3',
+#    }
+#}
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+'default': {
+            'ENGINE': 'django.db.backends.mysql',
+            'NAME': 'kitosgem$KanQ',
+            'USER': 'kitosgem',
+            'PASSWORD': 'grupo123',
+            'HOST': 'kitosgem.mysql.pythonanywhere-services.com',
+            'PORT': '3306',
+        }
 }
 
 
