4140c86ec1891fc9c18f7a4e98efebaa9f5f73ee
Marcos Otermin <marcosotermin@hotmail.com>
2023-12-11 19:56:04 -0300

    nuevo CORS

--- kanq/kanq/settings.py
+++ kanq/kanq/settings.py
@@ -45,8 +45,7 @@ INSTALLED_APPS = [
 
 #INSTALLED_APPS += CUSTOM_APP 
 
-MIDDLEWARE = [
-    #'app_store.cors.CorsMiddlewareMixin',
+MIDDLEWARE = [  
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -54,6 +53,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'app_store.cors.CorsMiddlewareMixin',
 ]
 
 ROOT_URLCONF = 'kanq.urls'
